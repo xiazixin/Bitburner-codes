4c3fe625aee3f6b20708c90b060ec5eac966c403
xiazixin <xia.zixin@yahoo.com>
2026-01-05 01:35:59 +0800

    bugfix: Remove infinite loop in main function of autonuke.js

--- autonuke.js
+++ autonuke.js
@@ -1,8 +1,7 @@
 export async function main(ns) {
     ns.args;
-    while (true) {
-        ns.scan(ns.args[0]) = scanresults;
-        ns.tprint("Scanned " + scanresults.length + " servers.");
-        ns.tprint(scanresults);
-    }
+    ns.scan(ns.args[0]) = scanresults;
+    ns.tprint("Scanned " + scanresults.length + " servers.");
+    ns.tprint(scanresults);
+    
 }
