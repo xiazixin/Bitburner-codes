55262b2cd7443adcdd050955436f8ec5ee19620b
xiazixin <xia.zixin@yahoo.com>
2026-01-04 22:45:34 +0800

    get nfo

--- getinfo.js
+++ getinfo.js
@@ -1,4 +1,6 @@
 export async function main(ns) {
     const args = ns.args;
-
+    ns.tprint(`Number of arguments: ${args.length}`);
+    ns.tprint(`First argument: ${args[0]}`);
+    ns.tprint(getServer(args[0]));
 }
