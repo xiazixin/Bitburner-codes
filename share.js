5f3f73910b63dbe9bf90da82b265ba21ab7750e2
xiazixin <xia.zixin@yahoo.com>
2026-01-05 01:13:04 +0800

    bugfix

--- share.js
+++ share.js
@@ -1,7 +1,7 @@
 export async function main(ns) {
     while (true) {
         const server = ns.args[0];
-        await ns.share(server);
+        await ns.share(); // Shares money with other players on the specified server
 }
 
 }
