81f176d0c74cdfbd5834ebb9d9177acce988297c
xiazixin <xia.zixin@yahoo.com>
2026-01-05 01:33:43 +0800

    feat: Add main function to autonuke.js for server scanning

---
+++ autonuke.js
@@ -0,0 +1,8 @@
+export async function main(ns) {
+    ns.args;
+    while (true) {
+        ns.scan(ns.args[0]) = scanresults;
+        ns.tprint("Scanned " + scanresults.length + " servers.");
+        ns.tprint(scanresults);
+    }
+}
