b15d37824f8b4b7192b079ba23714087d1945c31
xiazixin <xia.zixin@yahoo.com>
2026-01-04 22:50:17 +0800

    update

--- getinfo.js
+++ getinfo.js
@@ -2,11 +2,8 @@ export async function main(ns) {
     const args = ns.args;
     print = ns.tprint;
 
-    function getServer(serverName) {
-        const server = ns.getServer(serverName);
-        return `Server Name: ${server.name}, RAM: ${server.maxRam}GB, Hackable: ${server.hasAdminRights}`;
-    }
+
     ns.tprint(`Number of arguments: ${args.length}`);
     ns.tprint(`First argument: ${args[0]}`);
-    ns.tprint(getServer(args[0]));
+    ns.tprint(ns.getServer(args[0]));
 }
