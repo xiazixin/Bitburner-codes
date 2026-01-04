53e9e50d9a5d77ebfc42b0c7b9888c0dafa9d25e
xiazixin <xia.zixin@yahoo.com>
2026-01-05 01:37:01 +0800

    fix: Update logging message in main function of autonuke.js

--- autonuke.js
+++ autonuke.js
@@ -1,7 +1,7 @@
 export async function main(ns) {
     ns.args;
     ns.scan(ns.args[0]) = scanresults;
-    ns.tprint("Scanned " + scanresults.length + " servers.");
+    ns.tprint("Scanned servers.");
     ns.tprint(scanresults);
     
 }
