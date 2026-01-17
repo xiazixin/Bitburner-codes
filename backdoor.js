edea3f13be08cfb9e5cfc5a57c3b4d3755f8ec30
xiazixin <xia.zixin@yahoo.com>
2026-01-18 04:18:30 +0800

    Remove unused variables and improve backdoor.js script efficiency

--- backdoor.js
+++ backdoor.js
@@ -1,12 +1,12 @@
 export async function main(ns) {
     const servers =  ns.args.slice(0);
-    ns.brutessh(target);
-    ns.ftpcrack(target);
-    ns.relaysmtp(target);
-    ns.httpworm(target);
-    ns.sqlinject(target);
+    ns.brutessh(servers);
+    ns.ftpcrack(servers);
+    ns.relaysmtp(servers);
+    ns.httpworm(servers);
+    ns.sqlinject(servers);
     await ns.sleep(200);
-    ns.tprint(`Attempting to backdoor ${target}...`);
-    const success = ns.backdoor(target);
+    ns.tprint(`Attempting to backdoor ${servers}...`);
+    const success = ns.backdoor(servers);
 
 }
