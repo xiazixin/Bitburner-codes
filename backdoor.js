6b70c64941b589a29f1150033485093627e8a587
xiazixin <xia.zixin@yahoo.com>
2026-01-18 04:17:14 +0800

    fix: Correct namespace usage in backdoor.js script

--- backdoor.js
+++ backdoor.js
@@ -1,10 +1,10 @@
 export async function main(ns) {
     const servers =  ns.args.slice(0);
-    brutessh(target);
-    ftpcrack(target);
-    relaysmtp(target);
-    httpworm(target);
-    sqlinject(target);
+    ns.brutessh(target);
+    ns.ftpcrack(target);
+    ns.relaysmtp(target);
+    ns.httpworm(target);
+    ns.sqlinject(target);
     await ns.sleep(200);
     ns.tprint(`Attempting to backdoor ${target}...`);
     const success = ns.backdoor(target);
